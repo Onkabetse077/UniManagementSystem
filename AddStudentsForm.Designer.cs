77653f4f343d4f99f1140220e1af30f8805b0880
Onkabetse <113599354+Onkabetse077@users.noreply.github.com>
2024-10-31 10:54:22 +0200

    Needs Changes on the User Controls

--- AddStudentsForm.Designer.cs
+++ AddStudentsForm.Designer.cs
@@ -29,14 +29,26 @@
         private void InitializeComponent()
         {
             this.pnlStudentData_AddStudents = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.lblStudentData_AddStudents = new System.Windows.Forms.Label();
             this.dgvStudentData_AddStudents = new System.Windows.Forms.DataGridView();
-            this.lblStudentID_AddStudents = new System.Windows.Forms.Label();
+            this.lblStudentData_AddStudents = new System.Windows.Forms.Label();
+            this.pnlInputStudentData_AddStudents = new System.Windows.Forms.Panel();
+            this.tbAddress_AddStudent = new System.Windows.Forms.TextBox();
+            this.lblAddress_AddStudent = new System.Windows.Forms.Label();
+            this.tbStatus_AddStudents = new System.Windows.Forms.TextBox();
+            this.lblStatus_AddStudents = new System.Windows.Forms.Label();
+            this.tbSection_AddStudents = new System.Windows.Forms.TextBox();
+            this.lblSection_AddStudents = new System.Windows.Forms.Label();
+            this.tbGrade_AddStudent = new System.Windows.Forms.TextBox();
+            this.lblGrade_AddStudents = new System.Windows.Forms.Label();
+            this.lblGender_AddStudents = new System.Windows.Forms.Label();
+            this.cbGender_AddStudents = new System.Windows.Forms.ComboBox();
+            this.tbFullName_AddStudents = new System.Windows.Forms.TextBox();
+            this.lblFullName_AddStudents = new System.Windows.Forms.Label();
             this.tbStudentID_AddStudents = new System.Windows.Forms.TextBox();
+            this.lblStudentID_AddStudents = new System.Windows.Forms.Label();
             this.pnlStudentData_AddStudents.SuspendLayout();
-            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvStudentData_AddStudents)).BeginInit();
+            this.pnlInputStudentData_AddStudents.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlStudentData_AddStudents
@@ -50,16 +62,15 @@
             this.pnlStudentData_AddStudents.Size = new System.Drawing.Size(826, 296);
             this.pnlStudentData_AddStudents.TabIndex = 0;
             // 
-            // panel2
+            // dgvStudentData_AddStudents
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.tbStudentID_AddStudents);
-            this.panel2.Controls.Add(this.lblStudentID_AddStudents);
-            this.panel2.Location = new System.Drawing.Point(14, 337);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(826, 291);
-            this.panel2.TabIndex = 1;
+            this.dgvStudentData_AddStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudentData_AddStudents.Location = new System.Drawing.Point(20, 38);
+            this.dgvStudentData_AddStudents.Name = "dgvStudentData_AddStudents";
+            this.dgvStudentData_AddStudents.RowHeadersWidth = 51;
+            this.dgvStudentData_AddStudents.RowTemplate.Height = 24;
+            this.dgvStudentData_AddStudents.Size = new System.Drawing.Size(785, 243);
+            this.dgvStudentData_AddStudents.TabIndex = 1;
             // 
             // lblStudentData_AddStudents
             // 
@@ -71,47 +82,173 @@
             this.lblStudentData_AddStudents.TabIndex = 0;
             this.lblStudentData_AddStudents.Text = "Student Data";
             // 
-            // dgvStudentData_AddStudents
+            // pnlInputStudentData_AddStudents
             // 
-            this.dgvStudentData_AddStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvStudentData_AddStudents.Location = new System.Drawing.Point(20, 38);
-            this.dgvStudentData_AddStudents.Name = "dgvStudentData_AddStudents";
-            this.dgvStudentData_AddStudents.RowHeadersWidth = 51;
-            this.dgvStudentData_AddStudents.RowTemplate.Height = 24;
-            this.dgvStudentData_AddStudents.Size = new System.Drawing.Size(785, 243);
-            this.dgvStudentData_AddStudents.TabIndex = 1;
+            this.pnlInputStudentData_AddStudents.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pnlInputStudentData_AddStudents.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.tbAddress_AddStudent);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.lblAddress_AddStudent);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.tbStatus_AddStudents);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.lblStatus_AddStudents);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.tbSection_AddStudents);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.lblSection_AddStudents);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.tbGrade_AddStudent);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.lblGrade_AddStudents);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.lblGender_AddStudents);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.cbGender_AddStudents);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.tbFullName_AddStudents);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.lblFullName_AddStudents);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.tbStudentID_AddStudents);
+            this.pnlInputStudentData_AddStudents.Controls.Add(this.lblStudentID_AddStudents);
+            this.pnlInputStudentData_AddStudents.Location = new System.Drawing.Point(14, 337);
+            this.pnlInputStudentData_AddStudents.Name = "pnlInputStudentData_AddStudents";
+            this.pnlInputStudentData_AddStudents.Size = new System.Drawing.Size(826, 291);
+            this.pnlInputStudentData_AddStudents.TabIndex = 1;
+            // 
+            // tbAddress_AddStudent
+            // 
+            this.tbAddress_AddStudent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbAddress_AddStudent.Location = new System.Drawing.Point(355, 30);
+            this.tbAddress_AddStudent.Multiline = true;
+            this.tbAddress_AddStudent.Name = "tbAddress_AddStudent";
+            this.tbAddress_AddStudent.Size = new System.Drawing.Size(126, 70);
+            this.tbAddress_AddStudent.TabIndex = 14;
+            // 
+            // lblAddress_AddStudent
+            // 
+            this.lblAddress_AddStudent.AutoSize = true;
+            this.lblAddress_AddStudent.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress_AddStudent.Location = new System.Drawing.Point(265, 30);
+            this.lblAddress_AddStudent.Name = "lblAddress_AddStudent";
+            this.lblAddress_AddStudent.Size = new System.Drawing.Size(65, 18);
+            this.lblAddress_AddStudent.TabIndex = 13;
+            this.lblAddress_AddStudent.Text = "Address:";
+            // 
+            // tbStatus_AddStudents
+            // 
+            this.tbStatus_AddStudents.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbStatus_AddStudents.Location = new System.Drawing.Point(582, 82);
+            this.tbStatus_AddStudents.Name = "tbStatus_AddStudents";
+            this.tbStatus_AddStudents.Size = new System.Drawing.Size(126, 22);
+            this.tbStatus_AddStudents.TabIndex = 12;
+            // 
+            // lblStatus_AddStudents
+            // 
+            this.lblStatus_AddStudents.AutoSize = true;
+            this.lblStatus_AddStudents.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus_AddStudents.Location = new System.Drawing.Point(507, 82);
+            this.lblStatus_AddStudents.Name = "lblStatus_AddStudents";
+            this.lblStatus_AddStudents.Size = new System.Drawing.Size(54, 18);
+            this.lblStatus_AddStudents.TabIndex = 11;
+            this.lblStatus_AddStudents.Text = "Status:";
+            // 
+            // tbSection_AddStudents
+            // 
+            this.tbSection_AddStudents.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbSection_AddStudents.Location = new System.Drawing.Point(582, 26);
+            this.tbSection_AddStudents.Name = "tbSection_AddStudents";
+            this.tbSection_AddStudents.Size = new System.Drawing.Size(126, 22);
+            this.tbSection_AddStudents.TabIndex = 10;
+            // 
+            // lblSection_AddStudents
+            // 
+            this.lblSection_AddStudents.AutoSize = true;
+            this.lblSection_AddStudents.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSection_AddStudents.Location = new System.Drawing.Point(507, 30);
+            this.lblSection_AddStudents.Name = "lblSection_AddStudents";
+            this.lblSection_AddStudents.Size = new System.Drawing.Size(59, 18);
+            this.lblSection_AddStudents.TabIndex = 9;
+            this.lblSection_AddStudents.Text = "Section:";
+            // 
+            // tbGrade_AddStudent
+            // 
+            this.tbGrade_AddStudent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbGrade_AddStudent.Location = new System.Drawing.Point(355, 128);
+            this.tbGrade_AddStudent.Name = "tbGrade_AddStudent";
+            this.tbGrade_AddStudent.Size = new System.Drawing.Size(126, 22);
+            this.tbGrade_AddStudent.TabIndex = 8;
+            // 
+            // lblGrade_AddStudents
+            // 
+            this.lblGrade_AddStudents.AutoSize = true;
+            this.lblGrade_AddStudents.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade_AddStudents.Location = new System.Drawing.Point(265, 128);
+            this.lblGrade_AddStudents.Name = "lblGrade_AddStudents";
+            this.lblGrade_AddStudents.Size = new System.Drawing.Size(52, 18);
+            this.lblGrade_AddStudents.TabIndex = 7;
+            this.lblGrade_AddStudents.Text = "Grade:";
+            // 
+            // lblGender_AddStudents
+            // 
+            this.lblGender_AddStudents.AutoSize = true;
+            this.lblGender_AddStudents.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender_AddStudents.Location = new System.Drawing.Point(17, 122);
+            this.lblGender_AddStudents.Name = "lblGender_AddStudents";
+            this.lblGender_AddStudents.Size = new System.Drawing.Size(60, 18);
+            this.lblGender_AddStudents.TabIndex = 6;
+            this.lblGender_AddStudents.Text = "Gender:";
+            // 
+            // cbGender_AddStudents
+            // 
+            this.cbGender_AddStudents.FormattingEnabled = true;
+            this.cbGender_AddStudents.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Other"});
+            this.cbGender_AddStudents.Location = new System.Drawing.Point(107, 122);
+            this.cbGender_AddStudents.Name = "cbGender_AddStudents";
+            this.cbGender_AddStudents.Size = new System.Drawing.Size(126, 24);
+            this.cbGender_AddStudents.TabIndex = 5;
+            // 
+            // tbFullName_AddStudents
+            // 
+            this.tbFullName_AddStudents.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbFullName_AddStudents.Location = new System.Drawing.Point(107, 78);
+            this.tbFullName_AddStudents.Name = "tbFullName_AddStudents";
+            this.tbFullName_AddStudents.Size = new System.Drawing.Size(126, 22);
+            this.tbFullName_AddStudents.TabIndex = 4;
+            // 
+            // lblFullName_AddStudents
+            // 
+            this.lblFullName_AddStudents.AutoSize = true;
+            this.lblFullName_AddStudents.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFullName_AddStudents.Location = new System.Drawing.Point(17, 82);
+            this.lblFullName_AddStudents.Name = "lblFullName_AddStudents";
+            this.lblFullName_AddStudents.Size = new System.Drawing.Size(77, 18);
+            this.lblFullName_AddStudents.TabIndex = 3;
+            this.lblFullName_AddStudents.Text = "Full Name:";
+            // 
+            // tbStudentID_AddStudents
+            // 
+            this.tbStudentID_AddStudents.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbStudentID_AddStudents.Location = new System.Drawing.Point(107, 30);
+            this.tbStudentID_AddStudents.Name = "tbStudentID_AddStudents";
+            this.tbStudentID_AddStudents.Size = new System.Drawing.Size(126, 22);
+            this.tbStudentID_AddStudents.TabIndex = 2;
             // 
             // lblStudentID_AddStudents
             // 
             this.lblStudentID_AddStudents.AutoSize = true;
             this.lblStudentID_AddStudents.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStudentID_AddStudents.Location = new System.Drawing.Point(17, 38);
+            this.lblStudentID_AddStudents.Location = new System.Drawing.Point(17, 30);
             this.lblStudentID_AddStudents.Name = "lblStudentID_AddStudents";
             this.lblStudentID_AddStudents.Size = new System.Drawing.Size(84, 18);
             this.lblStudentID_AddStudents.TabIndex = 1;
             this.lblStudentID_AddStudents.Text = "Student ID:";
             // 
-            // tbStudentID_AddStudents
-            // 
-            this.tbStudentID_AddStudents.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tbStudentID_AddStudents.Location = new System.Drawing.Point(107, 34);
-            this.tbStudentID_AddStudents.Name = "tbStudentID_AddStudents";
-            this.tbStudentID_AddStudents.Size = new System.Drawing.Size(100, 22);
-            this.tbStudentID_AddStudents.TabIndex = 2;
-            // 
             // AddStudentsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlInputStudentData_AddStudents);
             this.Controls.Add(this.pnlStudentData_AddStudents);
             this.Name = "AddStudentsForm";
             this.Size = new System.Drawing.Size(855, 653);
             this.pnlStudentData_AddStudents.ResumeLayout(false);
             this.pnlStudentData_AddStudents.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvStudentData_AddStudents)).EndInit();
+            this.pnlInputStudentData_AddStudents.ResumeLayout(false);
+            this.pnlInputStudentData_AddStudents.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -119,10 +256,22 @@
         #endregion
 
         private System.Windows.Forms.Panel pnlStudentData_AddStudents;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel pnlInputStudentData_AddStudents;
         private System.Windows.Forms.Label lblStudentData_AddStudents;
         private System.Windows.Forms.DataGridView dgvStudentData_AddStudents;
         private System.Windows.Forms.TextBox tbStudentID_AddStudents;
         private System.Windows.Forms.Label lblStudentID_AddStudents;
+        private System.Windows.Forms.TextBox tbFullName_AddStudents;
+        private System.Windows.Forms.Label lblFullName_AddStudents;
+        private System.Windows.Forms.Label lblGender_AddStudents;
+        private System.Windows.Forms.ComboBox cbGender_AddStudents;
+        private System.Windows.Forms.TextBox tbStatus_AddStudents;
+        private System.Windows.Forms.Label lblStatus_AddStudents;
+        private System.Windows.Forms.TextBox tbSection_AddStudents;
+        private System.Windows.Forms.Label lblSection_AddStudents;
+        private System.Windows.Forms.TextBox tbGrade_AddStudent;
+        private System.Windows.Forms.Label lblGrade_AddStudents;
+        private System.Windows.Forms.TextBox tbAddress_AddStudent;
+        private System.Windows.Forms.Label lblAddress_AddStudent;
     }
 }
